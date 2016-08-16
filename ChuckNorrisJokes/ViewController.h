8b51315a3b4f3cc27c889d38a66068a49b1cda7c
Renemari Padillo <padillo.renemari@gmail.com>
2016-08-16 11:40:46 +0800

    Initial Commit

---
+++ ChuckNorrisJokes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChuckNorrisJokes
+//
+//  Created by Renesansz on 16/08/2016.
+//  Copyright © 2016 Renesansz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChuckNorrisJokesTests/ChuckNorrisJokesTests.m
@@ -0,0 +1,39 @@
+//
+//  ChuckNorrisJokesTests.m
+//  ChuckNorrisJokesTests
+//
+//  Created by Renesansz on 16/08/2016.
+//  Copyright © 2016 Renesansz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ChuckNorrisJokesTests : XCTestCase
+
+@end
+
+@implementation ChuckNorrisJokesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
